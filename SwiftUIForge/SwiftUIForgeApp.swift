f3e35d847b3c8ca72bcfaac3bac3d9a3967fa346
MV1998 <mv501049@gmail.com>
2026-01-25 18:49:42 +0530

    Initial SwiftUI project

---
+++ SwiftUIForge/SwiftUIForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIForgeApp.swift
+//  SwiftUIForge
+//
+//  Created by Mohit Varma on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
